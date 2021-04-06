3a38f7e485672ee373487793af5307c3b32ed0c0
Nugroho Adi Pratomo <nugrohoadi.pratomo24@gmail.com>
2021-04-06 23:36:56 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/script/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+
+        test: /\.s[ac]ss$/i,
+        use: [{
+            loader: 'css-loader',
+            options: {
+                url: false,
+            },
+        }],
+
+    },
+
+};
